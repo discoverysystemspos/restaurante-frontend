ad5d88e2fe2f538dc9361187958b1d3f990a6252
Gilmer Contreras <adgdiux@gmail.com>
2022-02-11 17:52:37 -0400

    Local

--- src/app/pages/facturas/factura/factura.component.ts
+++ src/app/pages/facturas/factura/factura.component.ts
@@ -5,6 +5,9 @@ import { Subscription, Observable } from 'rxjs';
 
 import Swal from 'sweetalert2';
 
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
 // PRINTER
 import { NgxPrinterService } from 'projects/ngx-printer/src/lib/ngx-printer.service';
 import { PrintItem } from 'projects/ngx-printer/src/lib/print-item';
@@ -93,7 +96,7 @@ export class FacturaComponent implements OnInit {
   }
 
   /** ================================================================
-   *   CARGAR FACTURA
+   *   CARGAR FACTURA a
   ==================================================================== */
   public idFactura;
   public iva: number = 0;
@@ -414,6 +417,34 @@ export class FacturaComponent implements OnInit {
           Swal.fire('Error', err.error.msg, 'error');
         });
 
+  }
+
+  /** ================================================================
+   *   PDF
+  ==================================================================== */
+  downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('printDiv') as HTMLElement;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas( DATA , options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}.pdf`);
+    });
   }
 
 
