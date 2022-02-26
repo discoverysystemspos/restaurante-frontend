1d0bfc34f3ed0f1dea5c40f2dfd44ed03ebc5e17
Gilmer Contreras <adgdiux@gmail.com>
2022-02-26 11:58:47 -0400

    Lan

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/api',
+  base_url: 'http://192.168.1.150:3000/api',
   local_url: 'https://castitoner.com/api'
 };
 
