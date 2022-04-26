3a0d281ab5ab837faf7b8348c9fee03c1a6e8df6
Gilmer Contreras <adgdiux@gmail.com>
2022-04-26 15:42:20 -0400

    Local

--- src/app/pages/productos/kardex/kardex.component.ts
+++ src/app/pages/productos/kardex/kardex.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ElementRef } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+
+// PRINTER
+import { NgxPrinterService } from 'projects/ngx-printer/src/lib/ngx-printer.service';
+import { PrintItem } from 'projects/ngx-printer/src/lib/print-item';
+import { ngxPrintMarkerPosition } from 'projects/ngx-printer/src/public_api';
 
 // MODELS
 import { Department } from '../../../models/department.model';
@@ -16,14 +22,39 @@ import { ProductService } from '../../../services/product.service';
 })
 export class KardexComponent implements OnInit {
 
+  @ViewChild('PrintTemplate')
+  private PrintTemplateTpl: TemplateRef<any>;
+
+  title = 'ngx-printer-demo';
+
+  printWindowSubscription: Subscription;
+  $printItems: Observable<PrintItem[]>;
+
   constructor(  private departmentService: DepartmentService,
-                private productsService: ProductService) { }
+                private productsService: ProductService,
+                private printerService: NgxPrinterService,) { 
+
+                  this.printWindowSubscription = this.printerService.$printWindowOpen.subscribe(
+                    val => {
+                      console.log('Print window is open:', val);
+                    }
+                  );
+              
+                  this.$printItems = this.printerService.$printItems;
+              }
 
   ngOnInit(): void {
 
     //CAARGAR DEPARTAMENTOS
     this.cargarDepartamnetos();
 
+  }
+
+  /** ================================================================
+   *   IMPRIMIR
+  ==================================================================== */
+  printDiv() {
+    this.printerService.printDiv('printDiv');
   }
 
   /** ===================================================================================
@@ -44,12 +75,30 @@ export class KardexComponent implements OnInit {
   /** ================================================================================
    * CARGAR PRODUCTOS
   ================================================================================== */
+  public desde: number = 0;
+  public endPoint: string;
   public productos: Product[] = [];
+
+  public cargando: boolean = false;
   
   buscar( termino: string ){
 
-    console.log(termino);
-    
+    this.cargando = true;
+
+    if (termino === 'Todos') {
+      return;
+    }else{
+      this.endPoint = `?desde=${this.desde}&tipo=none&status=false&departamento=${termino}`;
+    }
+
+
+    this.productsService.cargarProductos(this.endPoint)
+        .subscribe( ({ products }) => {
+
+          this.productos = products;
+          this.cargando = false;
+
+        });    
 
   }
 
